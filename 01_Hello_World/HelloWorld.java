870c46e6bb97d0d4f91beed658aac47a3b64c4a4
Karthik H <h.karthiknair@gmail.com>
2025-11-12 22:07:20 +0530

    arguments

--- 01_Hello_World/HelloWorld.java
+++ 01_Hello_World/HelloWorld.java
@@ -1,7 +1,14 @@
 
 
 class HelloWorld{
-    public static void main(String[] args) {
+    public static void main(String[] argument) {
         System.out.println("Hello world!");
+
+        // to convert the code into bytecode and pass it to jvm: javac HelloWorld.java
+        // to convert the bytecode to machine code and run the program: java HelloWorld
+        // anything written after command is included as a string of arguments starting with index 0.
+        // for ex: java HelloWorld arg0 arg1 arg2 ...
+        System.out.println(argument[0]);
+        System.out.println(argument[1]);
     }
 }
